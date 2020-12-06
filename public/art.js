8a46115384abcd45fa2b1956f66db7bfeb2a7288
stutz12 <71220547+stutz12@users.noreply.github.com>
2020-12-06 10:41:56 -0500

    Update art.js

--- public/art.js
+++ public/art.js
@@ -3,9 +3,11 @@ const Url ='http://jimskon.com:'+port
 var operation;
 var selectID;
 var recIndex;
+var textboxID;//
 var search;
 var password
-var artrecord = [];//stores each artwork locally. not implemented yet
+var artrecord = [];//stores each artwork locally
+var artpiecerecord = [] //stores URL of artwork locally
 var rows;
 var userinfoSelf = [];
 var sqlTable ="art.sql";
@@ -13,7 +15,7 @@ var isSelf = true; //for searching users, tells if it is self user or not;
 
 $(document).ready(function () {
     $(".modal").show();
-    $("#footer").hide(); //Wasn't looking right in login screen so I thought maybe just take it out.
+    $("#footer").hide();
     $("#modal-login-error-text").hide();//couldn't get it to be able to be hidden and then appear using hidden attribute on html so I moved it here
     $(".signup-modal").hide();
     $(".user-profile").hide();
@@ -34,7 +36,7 @@ $(document).ready(function () {
     $(".mainpage").click(function(e) {
       $("#results").html("");
       $("#art-search-input").val("");
-      $(".search-option").show();//junk, shoult not be in final product, just for testing.
+      $(".search-option").show();
       $(".user-profile").hide();
     });
 
@@ -56,7 +58,7 @@ $(document).ready(function () {
     addfavorite(artrecord[elmid]);
   });
 
- $(".profilebtn").click(function(){
+  $(".profilebtn").click(function(){
     console.log("Profile Show");
     $(".user-profile").show();
     $("#editbutton").show();
@@ -90,8 +92,9 @@ function errorText() {
   $("#modal-login-error-text").show();
 }
  function getList(){
+
    sqlTable="Userinfo.sql";
-   console.log($("#user-search-input").length);
+   //console.log($("#user-search-input").length);
    if($("#user-search-input").is(':visible')) {
    search=$("#user-search-input").val();
    $("#user-search-input").val(""); //clear user search box after the search
@@ -198,40 +201,39 @@ function processResults(results) {
         return;
       } else {
         console.log($("#password-login").val());
-	getFavorites(userinfo[0]);
+        getFavorites(userinfo[0]);
         $(".modal").hide();
         $(".user-profile").show();
         $("#user-username").text(userinfo[0]);
         $("#user-biography").text(userinfo[1]);
         $("#bioEdit").hide();
-        $("#changeBio").hide();
-        $("#cancelChange").hide();
+       $("#changeBio").hide();
+       $("#cancelChange").hide();
+       $("#editbutton").click(function(){
+        $("#bioEdit").show();
+        $("#changeBio").show();
+        $("#cancelChange").show();
 
-        $("#editbutton").click(function(){
-          $("#bioEdit").show();
-          $("#changeBio").show();
-          $("#cancelChange").show();
-          $("#bioEdit").val(userinfoSelf[1]);
-          $("#changeBio").click(function (){
-            newBio=$("#bioEdit").val();
-            $.ajax({
-              url: Url+'/editBio?Username='+userinfoSelf[0]+'&newBio='+newBio,
-              type:"GET",
-              success: processResults(results),
-              error: displayError,
-            })
-            $("#user-biography").text(newBio);
-            $("#bioEdit").hide();
-            $("#changeBio").hide();
-            $("#cancelChange").hide();
-          });
-          $("#cancelChange").click(function(){
-            $("#bioEdit").hide();
-            $("#changeBio").hide();
-            $("#cancelChange").hide();
-          })
-        });
-
+        $("#bioEdit").val(userinfoSelf[1]);
+        $("#changeBio").click(function (){
+          newBio=$("#bioEdit").val();
+        $.ajax({
+             url: Url+'/editBio?Username='+userinfoSelf[0]+'&newBio='+newBio,
+             type:"GET",
+             success: processResults(results),
+             error: displayError,
+           })
+           $("#user-biography").text(newBio);
+           $("#bioEdit").hide();
+           $("#changeBio").hide();
+           $("#cancelChange").hide();
+         });
+         $("#cancelChange").click(function(){
+           $("#bioEdit").hide();
+           $("#changeBio").hide();
+           $("#cancelChange").hide();
+         })
+       });
         for(var i=0; i<userinfo.length; i++) {
           console.log(userinfo.length);
           userinfoSelf[i]=userinfo[i];
@@ -245,11 +247,9 @@ function processResults(results) {
       console.log("When self is false");
       $(".search-option").hide();
       $(".user-profile").show();
-      $("#editbutton").hide();
       $("#user-username").text(userinfo[0]);
       $("#user-biography").text(userinfo[1]);
       getFavorites(userinfo[0]);
-      $(".favorite-artists").text(userinfo[3]);
     }
   }
   else if (sqlTable=="art.sql"){
@@ -265,28 +265,35 @@ function processResults(results) {
     result += '<h3>Results</h3>';
     var i=0;
     rows.forEach(function(row){
-        $.ajax({
-          url:Url+'/getcom?artpiece='+row.URL,
-          type:'GET',
-          error:displayError,
-        })
-      artrecord[i] = `<img src=${row.URL} class = URL>` + '<p class=Title>Title: ' + row.Title + '</p><p class = Year>Year: ' + row.Year + '</p><p class = Artist>Artist: ' + row.Artist + '</p><p class = Born>Artist Born-Died: ' + row.BornDied + '</p><p class = Technique>Technique: ' + row.Technique + '</p><p class = Location>Location: ' + row.Location + '</p><p class = Form>Form: ' + row.Form + '</p><p class = Type>Type: ' + row.Type + '</p><p class = School>School: ' + row.School + '</p><p class = Timeframe>Timeframe: ' + row.Timeframe + '</p>' ;
+      artrecord[i] = `<div class = "artResult" id="${i}"><img src=${row.URL} class = URL>` + '<p class=Title>Title: ' + row.Title + '</p><p class = Year>Year: ' + row.Year + '</p><p class = Artist>Artist: ' + row.Artist + '</p><p class = Born>Artist Born-Died: ' + row.BornDied + '</p><p class = Technique>Technique: ' + row.Technique + '</p><p class = Location>Location: ' + row.Location + '</p><p class = Form>Form: ' + row.Form + '</p><p class = Type>Type: ' + row.Type + '</p><p class = School>School: ' + row.School + '</p><p class = Timeframe>Timeframe: ' + row.Timeframe + '</p></div>' ;
       console.log(artrecord[i]);
-     result += artrecord[i] + '<p><button class ="list-add-button" data-id="'+ i + '">Add painting to favorites</button>';
-     result += '<div class="scrollabletextbox" id="message""></div><span id="chatinput"><textarea id="message" class="form-control" rows="1" cols="60" placehold="message"></textarea><br/><input type="button" value="send" id="send-btn"></p> </span>';
+      artpiecerecord[i]=row.URL;
+     result += artrecord[i] + '<button class ="list-add-button" data-id="'+ i + '">Add painting to favorites</button><button class="expand-comments-btn" data-id="' + i + '">See Comments</button><div class="scrollabletextbox" " id="com-'+i+'""></div><span id="chatinput"><textarea id="ta-'+i+'" class="form-control" rows="1" cols="60" placehold="message"></textarea><br/><input type="button" value="send" id="send-btn"></p> </span>';
+    $("#ta-"+i).hide();
      result += "<hr>";
      i++;
      //need to add in artrecord for choosing favorites
     })
+    $("#chatinput").hide();
+    $(".scrollabletextbox").hide();
     result += '</div>';
     $(result).appendTo('#results');
     console.log($(".list-add-button").length);
     $(".list-add-button").click(function(){
      console.log("favorite button clicked");
-     var elmid=$(this).attr("data-id");
+     var elmid = $(this).attr("data-id");
      console.log(elmid);
      addfavorite(artrecord[elmid]);
     });
+    $(".expand-comments-btn").click(function() {
+      console.log("expand comments list clicked");
+      textboxID=0;
+      var elmid=$(this).attr("data-id");
+      textboxID=elmid;
+      console.log(elmid);
+      getcomments(artpiecerecord[elmid]);
+    });
+
 }
 }
 }
@@ -294,7 +301,44 @@ function processResults(results) {
 function displayError(error) {
     console.log('Error ${error}');
 }
+function getcomments (commentpiece) {
+  console.log(commentpiece);
+  $.ajax({
+   url: Url+'/getcom?artpiece='+commentpiece,
+   type: "GET",
+   success: opencomments,
+   error: displayError,
+  })
+}
+function opencomments(results) {
+  console.log(results);
+  var comments = results.split(',');
+  var commentsParsed;
+  for(var i=0; i<comments.length; i++) {
+    if(i == 0) {
+      comments[i] = comments[i].replace('[{"','');
+    } else if (i == comments.length-1) {
+      comments[i] = comments[i].replace('"}]','');
+    }
+    comments[i]=comments[i].split('":"').pop();//removes column title from text
+    for(var j=0; j<comments[i].length; j++) {
+      comments[i][j]=comments[i][j].replace('""','');
+    }
+  }
+  for(var i=2; i<comments.length; i+=3) {
+    comments[i]=comments[i].split('T').shift();
+  }
+  for(var i=0; i<comments.length; i+=3) {
+    $(comments[i]+": "+comments[i+1]+"/br    "+comments[i+2]).appendTo("#com-"+textboxID);
+  }
+  console.log(textboxID);
+  $("#com-"+textboxID).show();
+  $("#chatinput").show();
+  $("#ta-"+textboxID).show();
+  console.log(comments);
 
+
+}
 
 function myFunction(imgs) {
   // Get the expanded image
@@ -315,11 +359,11 @@ function listFavorites(results){
         rows.forEach(function(row){
 	 result += row.artpiece;
 	})
-	$("#user-favs").html("<h3>Favorite Pieces</h3>");
-	if (result != undefined)
+  $("#user-favs").html("");
+  $("#user-favs").html("<h3>Favorite Pieces</h3>");
+  if (result != undefined)
 	$(result).appendTo("#user-favs");
 }
-
 function getFavorites(username){
 	$.ajax({
 	 url: Url+'/getfavs?Username='+username,
@@ -328,3 +372,4 @@ function getFavorites(username){
 	 error: displayError,
 	})
 }
+
